b5a87f56234c46930d91a688b8ea798ea960ea80
Adrian Leal <adleal956@gmail.com>
2022-10-29 21:09:11 -0700

    added coompleted form in modal on all pages

--- validateModal.js
+++ validateModal.js
@@ -1,18 +1,25 @@
-$(function () {
-	$('#newModalForm').validate({
-		rules: {
-			email1: {
-				required: true,
-				minlength: 8,
-			},
-			email1: 'required',
+(function () {
+	'use strict';
+	window.addEventListener(
+		'load',
+		function () {
+			// Fetch all the forms we want to apply custom Bootstrap validation styles to
+			var forms = document.getElementsByClassName('needs-validation');
+			// Loop over them and prevent submission
+			var validation = Array.prototype.filter.call(forms, function (form) {
+				form.addEventListener(
+					'submit',
+					function (event) {
+						if (form.checkValidity() === false) {
+							event.preventDefault();
+							event.stopPropagation();
+						}
+						form.classList.add('was-validated');
+					},
+					false
+				);
+			});
 		},
-		messages: {
-			email1: {
-				required: 'Please enter some data',
-				minlength: 'Your data must be at least 8 characters',
-			},
-			email1: 'Please provide some data',
-		},
-	});
-});
+		false
+	);
+})();
